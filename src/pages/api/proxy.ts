e794f20af2c498c2a64ba25b973dbb3128d34dd3
Create Something <micah@createsomething.io>
2024-06-25 15:59:47 -0500

    Production URL

--- src/pages/api/proxy.ts
+++ src/pages/api/proxy.ts
@@ -3,34 +3,30 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     try {
-      // Calculate the number of items added
-      const itemsAdded = Array.isArray(req.body.items) ? req.body.items.length : 0;
-
-      // Ensure the vendorID and itemsAdded are included in the request body
+      // Ensure the vendorID is included in the request body
       const requestBody = {
         ...req.body,
         vendorID: req.body.vendorID || 94, // Default to 94 if not provided
-        itemsAdded, // Set itemsAdded based on the length of the items array
       };
 
-      const response = await fetch('https://cato-supply-automation-system.onrender.com/webhook-test/2b81b67a-b719-49cc-ac97-560742ec7c53', {
+      const response = await fetch('https://cato-supply-automation-system.onrender.com/webhook/2b81b67a-b719-49cc-ac97-560742ec7c53', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(requestBody),
-      });
+      })
 
       if (response.ok) {
-        res.status(200).json({ message: 'Success' });
+        res.status(200).json({ message: 'Success' })
       } else {
-        res.status(response.status).json({ message: 'Failed to forward request' });
+        res.status(response.status).json({ message: 'Failed to forward request' })
       }
     } catch (error) {
-      console.error('Error forwarding request:', error);
-      res.status(500).json({ message: 'Internal server error' });
+      console.error('Error forwarding request:', error)
+      res.status(500).json({ message: 'Internal server error' })
     }
   } else {
-    res.status(405).json({ message: 'Method not allowed' });
+    res.status(405).json({ message: 'Method not allowed' })
   }
 }
